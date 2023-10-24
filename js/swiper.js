55633efae2a1f03503281dc6e855e02caae3a176
immanuelHirani <gabutb688@gmail.com>
2023-10-24 14:21:01 +0700

    Menyelesaikan Bagian Testimonial

---
+++ js/swiper.js
@@ -0,0 +1,33 @@
+let TestimonialSwiper = new Swiper(".testimonial-Content", {
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+  loop: true,
+  spaceBetween: 16,
+  grabCursor: true,
+  // If we need pagination
+
+  // Navigation arrows
+  navigation: {
+    nextEl: "#next",
+    prevEl: "#prev",
+  },
+
+  breakpoints: {
+    360: {
+      slidesPerView: 1.2,
+      centeredSlides: true,
+    },
+
+    600: {
+      slidesPerView: 1.2,
+      centeredSlides: true,
+    },
+    968: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+    },
+  },
+});
