198da8d72c31af4e995ac13bd60b7fddb33e0d1f
immanuelHirani <gabutb688@gmail.com>
2023-11-13 18:18:05 +0700

    p

---
+++ js/filternavSm.js
@@ -0,0 +1,17 @@
+const filterToggleSM = document.getElementById("filter-toggleSM");
+const filterToggleSm = document.querySelector(".filter-toggle-sm");
+const StoreItemSM = document.getElementById("StoreItemSM");
+
+const parentToggler = filterToggleSM.parentNode
+
+filterToggleSm.addEventListener('click' , () => {
+    filterToggleSM.classList.toggle('active-left');
+
+    if(filterToggleSM.classList.contains('active-left')) {
+        parentToggler.classList.add('active-parentBG')
+    } else {
+        parentToggler.classList.remove('active-parentBG')
+    }
+
+    console.log(parentToggler)
+})
