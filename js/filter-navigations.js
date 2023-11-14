d390ea226c09f300fd28d3df5f49da459bae4c35
immanuelHirani <gabutb688@gmail.com>
2023-11-14 18:14:51 +0700

    ::

--- js/filter-navigations.js
+++ js/filter-navigations.js
@@ -1,11 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const filterBoxToggle = document.getElementById("filter-box");
-  
-    filterBoxToggle.addEventListener("click", () => {
-      toggleClass(filterBoxToggle, "Filter-Active");
-  
-      const Box = filterBoxToggle.nextElementSibling;
-      toggleClass(Box, "box-Active");
-    });
+  const filterBoxToggle = document.getElementById("filter-box");
+  filterBoxToggle.addEventListener("click", () => {
+    filterBoxToggle.classList.toggle("Filter-Active");
+
+    Box = filterBoxToggle.nextElementSibling;
+    Box.classList.toggle("box-Active");
   });
-  
+});
+
+const overlayFilter = document.getElementById("overlay-Filter-Moderen");
+const filtertoggle = document.getElementById("filter-toggle");
+
+filtertoggle.addEventListener('click' , () => {
+    overlayFilter.classList.toggle('filter-overlay-active')
+})
