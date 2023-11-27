beb531202da0d3b174c761d27c12b0406a76a241
immanuelHirani <gabutb688@gmail.com>
2023-11-27 17:58:56 +0700

    Menyelesaikan Navbar Admin

--- js/admin.js
+++ js/admin.js
@@ -1,13 +1,28 @@
-const NavDrop = document.querySelectorAll('.NavDrop');
-const NavdropBox = document.querySelectorAll('.NavdropBox');
+const NavDrop = document.querySelector('.navdrop');
+const NavdropBoxes = document.querySelectorAll('.NavdropBox');
+const navBarAdmin = document.querySelectorAll('.navBarAdmin');
 
-const NavDropParent =  NavDrop.parentNode;
+navBarAdmin[0].classList.add('navAdmin-active');
 
-console.log(NavDropParent)
-
-NavDrop.forEach((OtherNavDrop, index) => {
-    OtherNavDrop.addEventListener("click", () => {
-        const parentNavdrop = NavdropBox[index].parentNode;
-        console.log(parentNavdrop);
-    }); 
+navBarAdmin.forEach(othernav => {
+    othernav.addEventListener('click', () => {
+        navBarAdmin.forEach(nav => {
+            if (nav !== othernav && nav.classList.contains('navAdmin-active')) {
+                nav.classList.remove('navAdmin-active');
+            }
+        });
+        othernav.classList.add('navAdmin-active');
+    });
 });
+
+NavDrop.addEventListener('click', () => {
+    const ParentBox = NavDrop.parentNode;
+    const NavdropNextElement = NavDrop.nextElementSibling;
+    NavDrop.classList.toggle('NavAdminToggle-active')
+    NavdropNextElement.classList.toggle('BoxNavAdmin-Active')
+    ParentBox.classList.toggle('parentAdminNav-Active')
+});
+
+
+
+
