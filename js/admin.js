cdbf534e0097c2a1e7fc2bc864c856766b2af58d
immanuelHirani <gabutb688@gmail.com>
2023-11-27 20:47:46 +0700

    ::

--- js/admin.js
+++ js/admin.js
@@ -1,28 +1,31 @@
-const NavDrop = document.querySelector('.navdrop');
-const NavdropBoxes = document.querySelectorAll('.NavdropBox');
-const navBarAdmin = document.querySelectorAll('.navBarAdmin');
+const NavDrop = document.querySelector(".navdrop");
+const NavdropBoxes = document.querySelectorAll(".NavdropBox");
+const navBarAdmin = document.querySelectorAll(".navBarAdmin");
 
-navBarAdmin[0].classList.add('navAdmin-active');
+const notifBox = document.querySelector(".notifBox");
+const notificationsToggle = document.getElementById("notifications");
 
-navBarAdmin.forEach(othernav => {
-    othernav.addEventListener('click', () => {
-        navBarAdmin.forEach(nav => {
-            if (nav !== othernav && nav.classList.contains('navAdmin-active')) {
-                nav.classList.remove('navAdmin-active');
-            }
-        });
-        othernav.classList.add('navAdmin-active');
+notificationsToggle.addEventListener("click", () => {
+  notifBox.classList.toggle("BoxNotifications-Active");
+});
+
+navBarAdmin[0].classList.add("navAdmin-active");
+
+navBarAdmin.forEach((othernav) => {
+  othernav.addEventListener("click", () => {
+    navBarAdmin.forEach((nav) => {
+      if (nav !== othernav && nav.classList.contains("navAdmin-active")) {
+        nav.classList.remove("navAdmin-active");
+      }
     });
+    othernav.classList.add("navAdmin-active");
+  });
 });
 
-NavDrop.addEventListener('click', () => {
-    const ParentBox = NavDrop.parentNode;
-    const NavdropNextElement = NavDrop.nextElementSibling;
-    NavDrop.classList.toggle('NavAdminToggle-active')
-    NavdropNextElement.classList.toggle('BoxNavAdmin-Active')
-    ParentBox.classList.toggle('parentAdminNav-Active')
+NavDrop.addEventListener("click", () => {
+  const ParentBox = NavDrop.parentNode;
+  const NavdropNextElement = NavDrop.nextElementSibling;
+  NavDrop.classList.toggle("NavAdminToggle-active");
+  NavdropNextElement.classList.toggle("BoxNavAdmin-Active");
+  ParentBox.classList.toggle("parentAdminNav-Active");
 });
-
-
-
-
