6522a33d79ddd8d355449f35ec8ec9dec5a6dfb7
immanuelHirani <gabutb688@gmail.com>
2023-12-13 18:47:08 +0700

    ::

---
+++ js/table.js
@@ -0,0 +1,156 @@
+$(document).ready(function () {
+    $("#myTable").DataTable({
+      responsive: true,
+      autoWidth: false, // Set autoWidth to false
+      lengthMenu: [
+        [3, 5, 10],
+        ["3", "5", "10"],
+      ], // Mengembalikan opsi 20, 50, 80, dan 100
+      language: {
+        // Konfigurasi bahasa lainnya
+        lengthMenu: "Tampilkan : _MENU_  per halaman",
+        zeroRecords: "Tidak ditemukan data yang sesuai",
+        info: "Menampilkan _PAGE_ dari _PAGES_ halaman",
+        infoEmpty: "Tidak ditemukan data yang sesuai",
+        infoFiltered: "(Disaring dari _MAX_ data keseluruhan)",
+        search: "Cari : ",
+        searchPlaceholder: "Masukkan kata kunci", // Menambahkan placeholder
+
+        paginate: {
+          previous: '<i class="ti ti-chevron-left"></i>',
+          next: '<i class="ti ti-chevron-right"></i>',
+        },
+      },
+    });
+  });
+
+  $(document).ready(function () {
+    $("#myTable2").DataTable({
+      responsive: true,
+      autoWidth: false, // Set autoWidth to false
+      lengthMenu: [
+        [4, 6, 10],
+        ["4", "6", "10"],
+      ], // Mengembalikan opsi 20, 50, 80, dan 100
+      language: {
+        // Konfigurasi bahasa lainnya
+        lengthMenu: "Tampilkan : _MENU_  per halaman",
+        zeroRecords: "Tidak ditemukan data yang sesuai",
+        info: "Menampilkan _PAGE_ dari _PAGES_ halaman",
+        infoEmpty: "Tidak ditemukan data yang sesuai",
+        infoFiltered: "(Disaring dari _MAX_ data keseluruhan)",
+        search: "Cari : ",
+        searchPlaceholder: "Masukkan kata kunci", // Menambahkan placeholder
+
+        paginate: {
+          previous: '<i class="ti ti-chevron-left"></i>',
+          next: '<i class="ti ti-chevron-right"></i>',
+        },
+      },
+    });
+  });
+
+  $(document).ready(function () {
+    $("#myTable3").DataTable({
+      responsive: true,
+      autoWidth: false, // Set autoWidth to false
+      lengthMenu: [
+        [5, 10],
+        ["5", "10"],
+      ], // Mengembalikan opsi 20, 50, 80, dan 100
+      language: {
+        // Konfigurasi bahasa lainnya
+        lengthMenu: "Tampilkan : _MENU_  per halaman",
+        zeroRecords: "Tidak ditemukan data yang sesuai",
+        info: "Menampilkan _PAGE_ dari _PAGES_ halaman",
+        infoEmpty: "Tidak ditemukan data yang sesuai",
+        infoFiltered: "(Disaring dari _MAX_ data keseluruhan)",
+        search: "Cari : ",
+        searchPlaceholder: "Masukkan kata kunci", // Menambahkan placeholder
+
+        paginate: {
+          previous: '<i class="ti ti-chevron-left"></i>',
+          next: '<i class="ti ti-chevron-right"></i>',
+        },
+      },
+    });
+  });
+
+  $(document).ready(function () {
+    $("#myTable4").DataTable({
+      responsive: true,
+      autoWidth: false, // Set autoWidth to false
+      lengthMenu: [
+        [5, 10 , 20],
+        ["5", "10" , "20"],
+      ], // Mengembalikan opsi 20, 50, 80, dan 100
+      language: {
+        // Konfigurasi bahasa lainnya
+        lengthMenu: "Tampilkan : _MENU_  per halaman",
+        zeroRecords: "Tidak ditemukan data yang sesuai",
+        info: "Menampilkan _PAGE_ dari _PAGES_ halaman",
+        infoEmpty: "Tidak ditemukan data yang sesuai",
+        infoFiltered: "(Disaring dari _MAX_ data keseluruhan)",
+        search: "Cari : ",
+        searchPlaceholder: "Masukkan kata kunci", // Menambahkan placeholder
+
+        paginate: {
+          previous: '<i class="ti ti-chevron-left"></i>',
+          next: '<i class="ti ti-chevron-right"></i>',
+        },
+      },
+    });
+  });
+
+
+  $(document).ready(function () {
+    $("#myTable5").DataTable({
+      responsive: true,
+      autoWidth: false, // Set autoWidth to false
+      lengthMenu: [
+        [3, 5, 10],
+        ["3", "5", "10"],
+      ], // Mengembalikan opsi 20, 50, 80, dan 100
+      language: {
+        // Konfigurasi bahasa lainnya
+        lengthMenu: "Tampilkan : _MENU_  per halaman",
+        zeroRecords: "Tidak ditemukan data yang sesuai",
+        info: "Menampilkan _PAGE_ dari _PAGES_ halaman",
+        infoEmpty: "Tidak ditemukan data yang sesuai",
+        infoFiltered: "(Disaring dari _MAX_ data keseluruhan)",
+        search: "Cari : ",
+        searchPlaceholder: "Masukkan kata kunci", // Menambahkan placeholder
+
+        paginate: {
+          previous: '<i class="ti ti-chevron-left"></i>',
+          next: '<i class="ti ti-chevron-right"></i>',
+        },
+      },
+    });
+  });
+
+  $(document).ready(function () {
+    $("#myTable6").DataTable({
+      responsive: true,
+      autoWidth: false, // Set autoWidth to false
+      lengthMenu: [
+        [3, 5, 10],
+        ["3", "5", "10"],
+      ], // Mengembalikan opsi 20, 50, 80, dan 100
+      language: {
+        // Konfigurasi bahasa lainnya
+        lengthMenu: "Tampilkan : _MENU_  per halaman",
+        zeroRecords: "Tidak ditemukan data yang sesuai",
+        info: "Menampilkan _PAGE_ dari _PAGES_ halaman",
+        infoEmpty: "Tidak ditemukan data yang sesuai",
+        infoFiltered: "(Disaring dari _MAX_ data keseluruhan)",
+        search: "Cari : ",
+        searchPlaceholder: "Masukkan kata kunci", // Menambahkan placeholder
+
+        paginate: {
+          previous: '<i class="ti ti-chevron-left"></i>',
+          next: '<i class="ti ti-chevron-right"></i>',
+        },
+      },
+    });
+  });
